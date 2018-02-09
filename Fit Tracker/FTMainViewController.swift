0ca17a794ceedb18b6ba04a7e5f5086e5c55a771
Casey McLewin <mclewinc@gmail.com>
2018-02-08 22:29:23 -0800

    Prelim db

--- Fit Tracker/FTMainViewController.swift
+++ Fit Tracker/FTMainViewController.swift
@@ -1,12 +1,16 @@
 //
-//  Exercise.swift
+//  ViewController.swift
 //  Fit Tracker
 //
 //  Created by Casey McLewin on 2018-01-29.
 //  Copyright © 2018 Casey McLewin. All rights reserved.
 //
 
-import Foundation
-import RealmSwift
+import UIKit
 
+class MainViewController: UIViewController {
+    
+    
+    
+}
 
